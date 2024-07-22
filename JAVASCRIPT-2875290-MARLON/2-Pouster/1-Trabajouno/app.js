6fa536c88b10010c4c29052a0d36c15664cb590c
CLAV890 <cmilo890@outlook.com>
2024-07-22 14:34:31 -0500

    Add files via upload

---
+++ JAVASCRIPT-2875290-MARLON/2-Pouster/1-Trabajouno/app.js
@@ -0,0 +1,21 @@
+//div.nube*4.
+const bolaf= document.getElementById('fuego')
+
+
+
+function play(){
+
+    bolaf.style.animationName= 'move'
+    bolaf.style.animationName= 'rotate'
+}
+
+
+bolaf.addEventListener('click',play)
+function play(){
+
+    bolaf.style.animationName= 'move'
+    bolaf.style.animationName= 'rotate'
+}
+
+
+bolaf.addEventListener('click',play)
